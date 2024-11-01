74a022ac8095b37690e18407409aa3137f819d5b
prem-idp <premkumar.mannu@idp.com>
2024-11-02 00:53:19 +0530

    seventh commit

--- src/app/utils/customfonts.tsx
+++ src/app/utils/customfonts.tsx
@@ -3,8 +3,8 @@ import customFonts from "next/font/local"
 // Method 1
 export const farroMedium = customFonts({
   src: [
-      {path: '../../../public/assets/fonts/Farro-Medium.woff2'},
-      {path: '../../../public/assets/fonts/Farro-Medium.woff'}],
+      {path: '../../../public/assets/fonts/Farro-Medium.woff'},
+      {path: '../../../public/assets/fonts/Farro-Medium.woff2'}],
     weight: '500',
     fallback: ['sans-serif'],
     variable: "--farro-medium",
@@ -12,8 +12,8 @@ export const farroMedium = customFonts({
 });
 export const farroBold = customFonts({
   src: [
-    {path: '../../../public/assets/fonts/Farro-Bold.woff2'},
-    {path: '../../../public/assets/fonts/Farro-Bold.woff'}],
+    {path: '../../../public/assets/fonts/Farro-Bold.woff'},
+    {path: '../../../public/assets/fonts/Farro-Bold.woff2'}],
     weight: '700',
     fallback: ['sans-serif'],
     variable: "--farro-bold",
@@ -21,8 +21,8 @@ export const farroBold = customFonts({
 });
 export const interRegular = customFonts({
   src: [
-    {path: '../../../public/assets/fonts/Inter-Regular.woff2'},
-    {path: '../../../public/assets/fonts/Inter-Regular.woff'}],
+    {path: '../../../public/assets/fonts/Inter-Regular.woff'},
+    {path: '../../../public/assets/fonts/Inter-Regular.woff2'}],
     weight: '400',
     fallback: ['sans-serif'],
     variable: "--inter-regular",
@@ -30,8 +30,8 @@ export const interRegular = customFonts({
 });
 export const interSemibold = customFonts({
   src: [
-    {path: '../../../public/assets/fonts/Inter-SemiBold.woff2'},
-    {path: '../../../public/assets/fonts/Inter-SemiBold.woff'}],
+    {path: '../../../public/assets/fonts/Inter-SemiBold.woff'},
+    {path: '../../../public/assets/fonts/Inter-SemiBold.woff2'}],
     weight: '600',
     fallback: ['sans-serif'],
     variable: "--inter-semibold",
@@ -39,8 +39,8 @@ export const interSemibold = customFonts({
 });
 export const interBold = customFonts({
   src: [
-    {path: '../../../public/assets/fonts/Inter-Bold.woff2'},
-    {path: '../../../public/assets/fonts/Inter-Bold.woff'}],
+    {path: '../../../public/assets/fonts/Inter-Bold.woff'},
+    {path: '../../../public/assets/fonts/Inter-Bold.woff2'}],
     weight: '700',
     fallback: ['sans-serif'],
     variable: "--inter-bold",
